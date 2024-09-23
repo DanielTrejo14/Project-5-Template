03fd389d0a3f5cb76f54a4de2f17b5a7c113f773
Daniel Trejo <dwtrejo14@comcast.net>
2024-09-23 14:57:21 -0400

    Routes in progress

--- server/app.py
+++ server/app.py
@@ -3,12 +3,11 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
-from flask_restful import Resource
-
+from flask import request, jsonify, session, Flask
+from functools import wraps
 # Local imports
 from config import app, db, api
-# Add your model imports
+from models import User, Recipe, Review, Category
 
 
 # Views go here!
@@ -16,6 +15,47 @@ from config import app, db, api
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'user.id' not in session:
+            return jsonify({'error': 'You need to be logged in to access this resource'}), 403
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+@app.route('/users', method=['POST'])
+def create_user():
+    data = request.json
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
+
+
+    if not username or not email or not password:
+        return jsonify({'error': 'all field are required'}), 400
+    
+
+    if User.query.filter_by(email=email).first():
+        return jsonify({'error': 'Email already registered'}), 400
+    
+    user = User(username=username, email=email)
+    user.set_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({'message': 'User created successfully', 'user': user.serialize()}), 201
+
+
+
+@app.route('/login', method=['POST'])
+def login():
+    data = request.json
+    
+    
+
+
 
 
 if __name__ == '__main__':
